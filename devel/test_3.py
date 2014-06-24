8e60437f28be4f858cb6131ef0e9f6c1d0563cb2
osbornelab <matthew.battifarano@gmail.com>
2014-06-24 16:40:29 -0500

    housekeeping

--- devel/test_3.py
+++
@@ -1,6 +0,0 @@
-import stimulus
-
-args='--segment-duration 60 --window-speed 0 --ndots 400 --dot-size 1  --field-size 5 --speed 8 --angle-var 4'.split()
-
-trial=stimulus.Trial(*args)
-trial.generate
--- tmp.py
+++
@@ -1,6 +0,0 @@
-try:
-    while True:
-        pass
-except KeyboardInterrupt:
-    print 'boo!'
-    quit()
